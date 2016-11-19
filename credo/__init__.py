b5678c92eec83e5a503166a80bc238fec588837b
Jon Staples <jstaples@nextgxdx.com>
2016-11-19 00:27:23 +0000

    preliminary markdown renderer

--- credo/__init__.py
+++ credo/__init__.py
@@ -1,5 +1,4 @@
 import os
-
 __author__ = "Jon Staples"
 __version__ = "0.0.1"
 __project_root__ = os.path.dirname(os.path.abspath(__file__))
@@ -7,4 +6,7 @@ __beliefs__ = os.path.join(__project_root__, "beliefs")
 __feelings__ = os.path.join(__beliefs__, "feelings")
 __opinions__ = os.path.join(__beliefs__, "opinions")
 __stances__ = os.path.join(__beliefs__, "stances")
-
+__all_paths__ = [
+    os.path.join(__beliefs__, dirname)
+    for dirname in os.listdir(__beliefs__)
+]
--- credo/main.py
+++ credo/main.py
@@ -1,12 +1,44 @@
+import os
 import yaml
+from credo import __all_paths__ 
+from toolz.itertoolz import concat
+from toolz.itertoolz import groupby
+
+def template(path):
+    return [
+        yaml.load(file(os.path.join(path, elem), "r"))
+        for elem in os.listdir(path)
+        if elem.endswith(".yaml")
+        or elem.endswith(".yml")
+    ]
 
 
-def feelings():
-    pass
+def render_markdown(belief_group):
+    _type = belief_group.get("type")
+    beliefs = "\n\n".join(["""
+## {0}
 
-def opinons():
-    pass
+### Summary
 
-def opinions():
-    pass
+{1}
+    """.format(elem["title"], elem["summary"])
+        for elem in belief_group["data"]
+    ])
+    print """
+# {0}    
+
+{1}
+    """.format(_type.title(), beliefs)
+
+
+def main():
+    all_beliefs = groupby("belief", list(concat(concat(map(template, __all_paths__)))))
+    by_type = sorted([dict(type=key, data=all_beliefs[key]) for key in all_beliefs])
+    for belief in by_type:
+        render_markdown(belief)
+
+
+if __name__ == "__main__":
+    main()
+
 
